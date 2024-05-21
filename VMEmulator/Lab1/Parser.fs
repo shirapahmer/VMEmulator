cd82c17f696b00f2a72135555f12c4285944fb6c
Jessica Zemble <jessicafeinmesser@gmail.com>
2024-05-21 21:44:28 +0300

    fixed things

--- VMEmulator/Lab1/Parser.fs
+++ VMEmulator/Lab1/Parser.fs
@@ -33,8 +33,10 @@ let find_substring (str1:string) = if str1.Contains(".vm") then str1 else null
 let vm_files = sub_file_names |> Array.map (fun name -> find_substring name)
 let filtered = vm_files |> Array.filter(String.IsNullOrEmpty >> not) //.vm files stored in filtered
 
-
-
+let convertToInt(str:string)=
+    let mutable result = 0
+    Int32.TryParse(str, &result)
+    result
     
 let mutable counter = 0
 
@@ -57,20 +59,28 @@ let handleOr() =
     streamWriter.WriteLine("@SP\nD=M-1\nA=D\nD=M\nA=A-1\nD=M|D\nM=D\nD=A+1\n@SP\nM=D")
 
 let handleEq() = 
-    streamWriter.WriteLine("@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "D=D-M\n" + "@EQUAL\n" + 
-    "D;JEQ\n" + "@NOTEQUAL\n" + "0;JMP\n" + "(EQUAL)\n" + "@LOADD\n" + "D=-1\n" + "0;JMP\n" + "(NOTEQUAL)\n" +
-    "@LOADD\n" + "D=0\n" + "0;JMP\n" + "(LOADD)\n" + "@SP\n" + "A=M\n"+"M=D\n"+"@SP\n" + "M=M+1\n")
+    counter <- counter+1
+    let stringCounter = Convert.ToString(counter)
+    streamWriter.WriteLine("@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "D=D-M\n" + "@EQL" + stringCounter
+    + "\n" + "D;JEQ\n" + "@NOTEQUAL" + stringCounter + "\n" + "0;JMP" + "\n" + "(EQL" + stringCounter + ")\n" + "@LOADDEQ" + stringCounter + "\n" + "D=-1\n" + "0;JMP\n" + "(NOTEQUAL" + stringCounter + ")\n" +
+    "@LOADDEQ" + stringCounter + "\n" + "D=0\n" + "0;JMP\n" + "(LOADDEQ" + stringCounter + ")\n" + "@SP\n" + "A=M\n"+"M=D\n"+"@SP\n" + "M=M+1")
 
 
 let handleGt() =
-    streamWriter.WriteLine("@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "D=M-D\n" + "@GREATERTHAN\n" + 
-    "D;JGT\n" + "@LESSTHAN\n" + "0;JMP\n" + "(GREATERTHAN)\n" + "@LOADD\n" + "D=-1\n" + "0;JMP\n" + "(LESSTHAN)\n" +
-    "@LOADD\n" + "D=0\n" + "0;JMP\n" + "(LOADD)\n" + "@SP\n" + "A=M\n"+"M=D\n"+"@SP\n" + "M=M+1\n")
+    counter <- counter+1
+    let stringCounter = Convert.ToString(counter)
+    streamWriter.WriteLine("@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "D=M-D\n" + "@GREATERTHAN_" +  stringCounter + "\n" +
+    "D;JGT\n" + "@LESSTHAN_"  + stringCounter + "\n" + "0;JMP\n" + "(GREATERTHAN_" + stringCounter + ")\n" + "@LOADDGT"  + stringCounter + "\n" 
+    + "D=-1\n" + "0;JMP\n" + "(LESSTHAN_" + stringCounter + ")\n" + "@LOADDGT"  + stringCounter + "\n" + "D=0\n" + "0;JMP\n" + "(LOADDGT" 
+    + stringCounter + ")\n" + "@SP\n" + "A=M\n"+"M=D\n"+"@SP\n" + "M=M+1")
 
 let handleLt() = 
-    streamWriter.WriteLine("@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "D=D-M\n" + "@GREATERTHAN\n" + 
-    "D;JGT\n" + "@LESSTHAN\n" + "0;JMP\n" + "(GREATERTHAN)\n" + "@LOADD\n" + "D=-1\n" + "0;JMP\n" + "(LESSTHAN)\n" +
-    "@LOADD\n" + "D=0\n" + "0;JMP\n" + "(LOADD)\n" + "@SP\n" + "A=M\n"+"M=D\n"+"@SP\n" + "M=M+1\n")
+    counter <- counter+1
+    let stringCounter = Convert.ToString(counter)
+    streamWriter.WriteLine("@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "D=D-M\n" + "@GREATERTHAN"  + stringCounter + "\n" + 
+    "D;JGT\n" + "@LESSTHAN"  + stringCounter + "\n" + "0;JMP\n" + "(GREATERTHAN"  + stringCounter + ")\n"+ "@LOADDLT"  + stringCounter +
+    "\n" + "D=-1\n" + "0;JMP\n" + "(LESSTHAN"  + stringCounter + ")\n" +
+    "@LOADDLT"  + stringCounter + "\n"+"D=0\n" + "0;JMP\n" + "(LOADDLT" + stringCounter + ")\n" + "@SP\n" + "A=M\n"+"M=D\n"+"@SP\n" + "M=M+1")
 
 let handlePushSegment (segment,index:string) = 
     let mutable new_seg = segment
@@ -102,20 +112,20 @@ let handlePopStatic index fileName =
     streamWriter.WriteLine("@"+fileNameI+"\nD=M\n@"+index+"\nD=D+A\n@"+fileNameI+"\nM=D\n@SP\nM=M-1\n@SP\nA=M\nD=M\n@"+fileNameI+"\nA=M\nM=D\n@"+fileNameI+"\nD=M\n@"+index+"\nD=D-A\n@"+fileNameI+"\nM=D")
 
 let handlePushTemp segment index =
-    let new_seg = "TEMP"
-    streamWriter.WriteLine("@"+new_seg+"\nD=M\n@"+index+"\nD=D+A\n@"+new_seg+"\nM=D\n@"+new_seg+"\nA=M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1")
+    let temp_index = Convert.ToString(convertToInt(index)+5)
+    streamWriter.WriteLine("@"+temp_index+"\nD=M\n@SP"+"\nA=M\n"+"A=M\n"+"M=D\n@SP\nM=M+1")
 
 let handlePopTemp segment index =
-    let new_seg = "TEMP"
-    streamWriter.WriteLine("@"+new_seg+"\nD=M\n@"+index+"\nD=D+A\n@"+new_seg+"\nM=D\n@SP\nM=M-1\n@SP\nA=M\nD=M\n@"+new_seg+"\nA=M\nM=D\n@"+new_seg+"\nD=M\n@"+index+"\nD=D-A\n@"+new_seg+"\nM=D")
+    let temp_index = Convert.ToString(convertToInt(index)+5)
+    streamWriter.WriteLine("@SP\nAM=M-1\nD=M\n@"+temp_index+"\nM=D")
 
 let handlePushPointer index = 
-    if index = "1" then streamWriter.WriteLine("@THAT\n" + "D=M\n" + "@SP\n" + "A=M\n" + "M=D\n" + "@SP" + "M=M+1\n") 
-    else if index = "0" then streamWriter.WriteLine("@THIS\n" + "D=M\n" + "@SP\n" + "A=M\n" + "M=D\n" + "@SP" + "M=M+1\n")
+    if index = "1" then streamWriter.WriteLine("@THAT\n" + "D=M\n" + "@SP\n" + "A=M\n" + "M=D\n" + "@SP\n" + "M=M+1") 
+    else if index = "0" then streamWriter.WriteLine("@THIS\n" + "D=M\n" + "@SP\n" + "A=M\n" + "M=D\n" + "@SP\n" + "M=M+1")
 
 let handlePopPointer index =  
-    if index = "1" then streamWriter.WriteLine("@SP\n" + "AM=M-1\n" +  "D=M\n" + "@THAT\n" + "M=D\n")
-    else if index = "0" then streamWriter.WriteLine("@SP\n" + "AM=M-1\n" +  "D=M\n" + "@THIS\n" + "M=D\n")
+    if index = "1" then streamWriter.WriteLine("@SP\n" + "AM=M-1\n" +  "D=M\n" + "@THAT\n" + "M=D")
+    else if index = "0" then streamWriter.WriteLine("@SP\n" + "AM=M-1\n" +  "D=M\n" + "@THIS\n" + "M=D")
 
 
 let handlePush(segment:string, index:string, fileName:string) = 
@@ -144,8 +154,13 @@ let check_command(fileName:string, command:string) =
     | "eq" -> handleEq()
     | "gt" -> handleGt()
     | "lt" -> handleLt()
+    | "or" -> handleOr()
+    | "and" -> handleAnd()
+    | "not" -> handleNot()
     | "push" -> handlePush(split[1], split[2], fileName)
     | "pop" -> handlePop(split[1], split[2], fileName)
+   
+
 
 let read_vm_file (file_name:string) =
     let file_path = path + "\\" + file_name
